801a58d1dd1812e6af9cd69ada74701675cab0c1
Paul Storch <sto.paul14@gmail.com>
2020-09-28 17:37:07 +0200

    adding: after six iterations the parentprocess kills the child

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <thread>
 #include <unistd.h>
+#include <csignal>
 
 using namespace std;
 
+
 int main() {
-    pid_t pid = fork();
+    pid_t pid{fork()};
 
     if( pid == 0){
-        while(true) {
-            cout << "B" << flush;
-            std::chrono::milliseconds sleeptime(500);
-            std::this_thread::sleep_for(sleeptime);
-        }
-    }
-    else if (pid > 0) {
         while(true) {
             cout << "A" << flush;
             std::chrono::milliseconds sleeptime(500);
             std::this_thread::sleep_for(sleeptime);
         }
+    }
+    else if (pid > 0) {
+        for(int count{0}; count < 6; count++) {
+            cout << "B" << flush;
+            std::chrono::milliseconds sleeptime(500);
+            std::this_thread::sleep_for(sleeptime);
+        }
+        kill(pid, SIGKILL);
     }
 }
