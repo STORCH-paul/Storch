c3dd5b8fa7f6da0f59c75de334d6c8878c325b3b
Paul Storch <sto.paul14@gmail.com>
2020-09-28 19:43:46 +0200

    adding monitoring cout's

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -18,26 +18,54 @@ int main() {
         if (pid1 == 0) {
             if (aba_letter_a) {
                 execl("./charout", "./charout", aba_letter_a, 0);
+                
+                if (errno) {
+                    cerr << "starting charout failed: " << strerror(errno) << endl;
+                    exit(1);  
+                }
             }else {
                 cout << "ABA_LETTER_A not set" << endl;
-            }   
+                exit(1);  
+            }
+        exit(0);   
         }
         else {
             pid_t pid2{fork()};
             if (pid2 == 0) {
                 if (aba_letter_b) {
                     execl("./charout", "./charout", aba_letter_b, 0);
+                    if (errno) {
+                        cerr << "starting charout failed: " << strerror(errno) << endl;
+                        exit(1);  
+                    }
+                    
                 }else {
                     cout << "ABA_LETTER_B not set" << endl;
-                }    
+                    exit(1);  
+                }  
+                exit(0);  
             }
 
+            cout << "waiting for 3 seconds" << endl;
             std::chrono::milliseconds sleeptime(3000);
             std::this_thread::sleep_for(sleeptime);
 
+            cout << "\n" << flush;
+
+            cout << "killing both subprocesses with pids " << pid1 << " and " << pid2 << endl;
+            cout << "waiting for both subprocesses to be dead" << endl;
+
             kill(pid1, SIGKILL);
             kill(pid2, SIGKILL);
+
+            int status1;
+            int status2;
+            waitpid(pid1, &status1, 0);
+            waitpid(pid2, &status2, 0);
+
+            cout << "subprocess " << pid1 <<" exited with "<< WEXITSTATUS(status1) << endl;
+            cout << "subprocess " << pid2 <<" exited with "<< WEXITSTATUS(status2) << endl;
+
             exit(EXIT_SUCCESS);
         }
-        sleep(100);
 }
