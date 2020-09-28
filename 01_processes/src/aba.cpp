ea38830f7e8f067c8b2db748bf4aa58e542d7d56
Paul Storch <sto.paul14@gmail.com>
2020-09-28 18:59:46 +0200

    adding systemvar.

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -4,24 +4,32 @@
 #include <csignal>
 #include <sys/wait.h>
 #include <cstring>
+#include <cstdlib>
 
 
 using namespace std;
 
 
 int main() {
+    const char* aba_letter_a{getenv("ABA_LETTER_A")};
+    const char* aba_letter_b{getenv("ABA_LETTER_B")};
 
     pid_t pid1{fork()};
-        if (pid1 == 0)
-        {
-            execl("./charout", "./charout", "A", 0);
+        if (pid1 == 0) {
+            if (aba_letter_a) {
+                execl("./charout", "./charout", aba_letter_a, 0);
+            }else {
+                cout << "ABA_LETTER_A not set" << endl;
+            }   
         }
-        else
-        {
+        else {
             pid_t pid2{fork()};
-            if (pid2 == 0)
-            {
-                execl("./charout", "./charout", "B", 0);
+            if (pid2 == 0) {
+                if (aba_letter_b) {
+                    execl("./charout", "./charout", aba_letter_b, 0);
+                }else {
+                    cout << "ABA_LETTER_B not set" << endl;
+                }    
             }
 
             std::chrono::milliseconds sleeptime(3000);
