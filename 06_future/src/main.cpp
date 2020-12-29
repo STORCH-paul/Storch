d7edaf134f61abd17a4e967d3bf0b9ef19e40296
Paul Storch <sto.paul14@gmail.com>
2020-12-29 13:27:35 +0100

    implementing 2.

---
+++ 06_future/src/main.cpp
@@ -0,0 +1,54 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmisleading-indentation"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+#include "InfInt.h"
+#pragma GCC diagnostic pop
+#include <iostream>
+#include <future>
+#include <vector>
+#include <string>
+#include "calc_factors.h"
+#include "CLI11.hpp"
+
+using namespace std;
+
+string check_callable(const string& numbers){
+    size_t check{numbers.find_first_not_of("0123456789")};
+    if(check == string::npos){
+        return "";
+    }
+    else{
+        return string(numbers) + " contains not numeric character";
+    }  
+}
+
+int main(int argc, char *argv[]){
+    CLI::App app("Factor numbers");
+    vector<string> numbers{};
+    app.add_option("number", numbers, "numbers to factor")->required()->check(check_callable);
+    CLI11_PARSE(app, argc, argv);
+    vector<InfInt> futs{};
+    for (auto num : numbers)
+    {
+        InfInt number = num;
+        cout << number.toString() << ": ";
+        for (auto primes : get_factors(InfInt(number)))
+        {
+            cout << primes.toString() << " ";
+        }
+        cout << endl;
+    }
+
+
+/*     vector<future<vector<InfInt>>> futs{};
+    for (auto num : numbers)
+    {
+        InfInt number = num;
+        futs.push_back(async([&]() {return get_factors(InfInt(number));}));
+    }
+
+    for (auto& elem : futs)
+    {
+        auto primes = elem.get();
+    } */
+}
